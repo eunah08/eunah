9d1c9c3bf9bd86a765ecba55737101bc75b187f0
eunah08 <a01046634221@gmail.com>
2026-08-28 20:19:26 +0900

    5번

--- week2/1. basic/05_recursion.py
+++ week2/1. basic/05_recursion.py
@@ -32,11 +32,15 @@ def factorial(n):
     Returns:
         n의 팩토리얼 값
     """
-    # TODO: base case를 작성하세요
-    # n이 0이거나 1이면 1을 반환
-    pass
-    
-    # TODO: recursive case를 작성하세요
+    # TODO: base case를 작성하세요 (탈출 조건)
+    # 만약 n=0 or 1 이면 1 반환
+    if n==0 or n==1:
+            return 1
+
+    # TODO: recursive case를 작성하세요 (재귀조건)
+    else:
+        return n * factorial(n - 1)
+
     pass
 
 def fibonacci(n):
@@ -50,12 +54,15 @@ def fibonacci(n):
         n번째 피보나치 수
     """
     # TODO: base case를 작성하세요
-    # n이 0이면 0, n이 1이면 1 반환
-    pass
-    
-    # TODO: recursive case를 작성하세요
-    pass
+        # n이 0이면 0, n이 1이면 1 반환
+    if n <= 1:
+        return n
 
+    # TODO: recursive case를 작성하세요
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+    
 # 테스트 케이스
 if __name__ == "__main__":
     # 팩토리얼 테스트
