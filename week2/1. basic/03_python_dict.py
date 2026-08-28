7f1bf4ed4af2c61bcad25435515bf46d398d2a9b
eunah08 <a01046634221@gmail.com>
2026-08-28 16:32:12 +0900

    2번

--- week2/1. basic/03_python_dict.py
+++ week2/1. basic/03_python_dict.py
@@ -42,13 +42,23 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
+    scores = [student["score"] for student in students]    
     pass
     
     # TODO: 평균 점수를 계산하세요
+    total = sum(student["score"] for student in students)
+    people = len(students)
+    average = total / people
+
+    print(average)
     pass
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    pass
+    above_average_students = []
+
+    for student in students:
+        if student["score"] >= average:
+            above_average_students.append(student["name"])
     
     return average, above_average_students
 
